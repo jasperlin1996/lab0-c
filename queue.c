8593855d8d01df19757d82e220d6d1cbb42f6fdc
Jasper Lin <jasperlin1996@gmail.com>
2022-02-21 20:21:59 +0000

    Leveraging slow-fast pointer on `q_delete_mid`
    Implement `q_delete_mid` via a slow-fast pointer technique. This
    technique could help us find the mid node when the "fast pointer"
    reach the end of the queue.

--- queue.c
+++ queue.c
@@ -219,6 +219,18 @@ int q_size(struct list_head *head)
 bool q_delete_mid(struct list_head *head)
 {
     // https://leetcode.com/problems/delete-the-middle-node-of-a-linked-list/
+    if (!head || list_empty(head))
+        return false;
+
+    struct list_head **indirect = &(head->next);
+    for (struct list_head *fast = head->next;
+         fast != head && fast->next != head; fast = fast->next->next) {
+        indirect = &(*indirect)->next;
+    }
+    struct list_head *tmp = *indirect;
+    list_del(tmp);
+    q_release_element(list_entry(tmp, element_t, list));
+
     return true;
 }
 
