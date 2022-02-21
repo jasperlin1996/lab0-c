1caac7fdf750486cf7c012e7cfeb01b1b26df71c
Jasper Lin <jasperlin1996@gmail.com>
2022-02-21 20:16:12 +0000

    Implement `q_reverse` by swapping the pointers
    Swapping the `next` and `prev` pointer of all nodes can reverse the
    queue.

--- queue.c
+++ queue.c
@@ -252,7 +252,20 @@ void q_swap(struct list_head *head)
  * (e.g., by calling q_insert_head, q_insert_tail, or q_remove_head).
  * It should rearrange the existing ones.
  */
-void q_reverse(struct list_head *head) {}
+void q_reverse(struct list_head *head)
+{
+    if (!head || head->next == head)
+        return;
+    struct list_head *node = head;
+    // Swap all node's next and prev pointer
+    do {
+        struct list_head *tmp = node->next;
+        node->next = node->prev;
+        node->prev = tmp;
+
+        node = node->prev;
+    } while (node != head);
+}
 
 /*
  * Sort elements of queue in ascending order
